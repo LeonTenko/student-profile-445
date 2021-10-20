bc7715e9fd3da8b2086b45e6a666efadf38f1b26
Leon <radraptorleo@gmail.com>
2021-10-20 16:23:00 -0700

    Fetch Data From Provided API
    - Data gets fetched and stored into an array.
    - console.log the data for initial testing.
    - All data gets fetched correctly.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [studentData, setStudentData] = React.useState([]);
+
+  const url = new URL("https://api.hatchways.io/assessment/students");
+  const fetchParams = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default'
+  };
+
+  const getStudentData = async () => {
+    let response = await fetch(url, fetchParams);
+
+    let responseJSON = await response.json();
+
+    let responseArray = responseJSON['students'];
+
+    setStudentData(responseArray);
+  };
+
+  React.useEffect(() => {
+    getStudentData();
+  }, []);
+
+  React.useEffect(() => {
+    console.log(studentData);
+  }, [studentData]);
+
   return (
     <div className="App">
-      <h1>
-        Test
-      </h1>
+      <p>
+      </p>
     </div>
   );
 }
