41de8e441d1134b0d87bba0011f064706db32ce1
Leon <radraptorleo@gmail.com>
2021-10-21 10:40:24 -0700

    Add Scrollbar to Student Card
    - Stylized the scrollbar using ::-webkit-scrollbar to match the
    video.
    - Scrolling works as expected.

--- src/App.js
+++ src/App.js
@@ -12,11 +12,24 @@ const useStyles = makeStyles({
     width: "100vw",
     height: "100vh",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    overflow: "hidden"
   },
   mainContents: {
     minWidth: "60%",
-    height: "75%"
+    height: "75%",
+    overflowY: "auto",
+    "&::-webkit-scrollbar": {
+      width: "0.4em"
+    },
+    '&::-webkit-scrollbar-track': {
+      boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+      webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)'
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'rgba(0,0,0,.1)',
+      outline: '1px solid slategrey'
+    }
   }
 });
 
