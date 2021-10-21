1d30ee3bb14ee26cdb93eb175944d7557c086dba
Leon <radraptorleo@gmail.com>
2021-10-20 19:13:45 -0700

    Provide Initial CSS Layout For Student Components
    - Added makeStyle MaterialUI component for MUI Styling.
    - Provided the basic grid layout of the student components.

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,4 +1,32 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: "100%",
+    border: "1px solid black",
+    padding: "2vh"
+  },
+  gridAvatar: {
+    width: "100%",
+    height: "100%",
+    border: "1px solid blue",
+    "& img": {
+      width: "100%",
+      height: "100%",
+      objectFit: "contain",
+      borderRadius: "50%",
+      border: "1px solid #C0C0C0"
+
+    }
+  },
+  gridContents: {
+    width: "auto",
+    height: "100%",
+    border: "1px solid green"
+  }
+});
 
 const Student = ({
   city,
@@ -10,6 +38,7 @@ const Student = ({
   lastName,
   pic,
   skill }) => {
+  const classes = useStyles();
 
   const calcAverage = (grades) => {
     const grd = grades["grades"].map((str) => parseInt(str));
@@ -19,13 +48,23 @@ const Student = ({
   };
 
   return (
-    <div>
-      <img src={pic} />
-      <h1>{firstName} {lastName}</h1>
+    <div className={classes.root}>
+      <Grid container spacing={0}>
+        <Grid item xs={3} >
+          <div className={classes.gridAvatar}>
+            <img src={pic} alt="avatar" />
+          </div>
+        </Grid>
+        <Grid item xs={9}>
+          <div className={classes.gridContents}>
+          </div>
+        </Grid>
+      </Grid>
+      {/* <h1>{firstName} {lastName}</h1>
       <p>Email: {email}</p>
       <p>Company: {company}</p>
       <p>Skill: {skill}</p>
-      <p>Average: {calcAverage({ grades })}</p>
+      <p>Average: {calcAverage({ grades })}</p> */}
     </div>
   );
 };
